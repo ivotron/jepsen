92160238b947c79af3d5a5ec9e338c2e9d8b366d
Ivo Jimenez <ivo.jimenez@gmail.com>
2016-11-23 10:16:32 -0800

    Removes SNAPSHOT from version IDs

--- ceph/project.clj
+++ ceph/project.clj
@@ -1,7 +1,7 @@
-(defproject jepsen.ceph "0.1.0-SNAPSHOT"
+(defproject jepsen.ceph "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [jepsen "0.1.3-SNAPSHOT"]])
+                 [jepsen "0.1.3"]])
