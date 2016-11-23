179a788742b76a623795e1f50f9cc785fe31794f
Ivo Jimenez <ivo.jimenez@gmail.com>
2016-11-23 11:43:36 -0800

    Adds client for set test
    Missing a checker

--- ceph/src/jepsen/ceph.clj
+++ ceph/src/jepsen/ceph.clj
@@ -1,6 +1,61 @@
 (ns jepsen.ceph
-  (:require [jepsen.tests :as tests]))
+  (:require [clojure.string :as str]
+            [jepsen [db         :as db]
+                    [checker    :as checker]
+                    [client     :as client]
+                    [control    :as c]
+                    [generator  :as gen]
+                    [model      :as model]
+                    [nemesis    :as nemesis]
+                    [tests      :as tests]
+                    [util :refer [timeout]]]))
+
+
+(defn r [_ _] {:type :invoke, :f :read, :value nil})
+(defn w [_ _] {:type :invoke, :f :write, :value (rand-int 5)})
+
+(defn mon-set!
+  "Set a value for key"
+  [node k v]
+  (c/on node
+     (c/exec :ceph :config-key :put k v)))
+
+(defn mon-get!
+  "Get a value for key"
+  [node k]
+  (c/on node
+     (c/exec :ceph :config-key :get k :-o :v)
+     (c/exec :cat :v)))
+
+(defn client
+  "A client for a get/set register"
+  [node k]
+  (reify client/Client
+    (setup! [_ test node]
+      (let [n node k "theregister"] (client n k)))
+
+    (invoke! [this test op]
+      (timeout 5000 (assoc op :type :info, :error :timeout)
+        (case (:f op)
+          :read  (let[resp (-> (mon-get! node k))]
+                   (assoc op :type :ok, :value resp))
+          :write ((mon-set! node k (:value op))
+                  (assoc op :type :ok)))))
+
+    (teardown! [_ test])))
 
 (defn mon-test
   []
-  tests/noop-test)
+  (assoc tests/noop-test
+     :name "cephmon"
+     :nemesis (nemesis/partition-random-halves)
+     :client (client nil nil)
+     :generator (->> (gen/mix [r w])
+                     (gen/stagger 1)
+                     (gen/nemesis
+                       (gen/seq (cycle [(gen/sleep 5)
+                                        {:type :info, :f :start}
+                                        (gen/sleep 5)
+                                        {:type :info, :f :stop}])))
+                     (gen/time-limit 60))
+     :model (model/set)))
