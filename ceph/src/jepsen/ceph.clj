e2cc67f35a0726fbc934a480c707aeff14e0b941
leosinclairJr <steven.y.zh@hotmail.com>
2016-11-30 19:48:15 -0800

    some error fixed

--- ceph/src/jepsen/ceph.clj
+++ ceph/src/jepsen/ceph.clj
@@ -8,7 +8,9 @@
                     [model      :as model]
                     [nemesis    :as nemesis]
                     [tests      :as tests]
-                    [util :refer [timeout]]]))
+                    [util :refer [timeout]]]
+	     [knossos.model      :as checkModel]
+))
 
 
 (defn r [_ _] {:type :invoke, :f :read, :value nil})
@@ -32,15 +34,17 @@
   [node k]
   (reify client/Client
     (setup! [_ test node]
-      (let [n node k "theregister"] (client n k)))
+      (let [n node k "keyRegister"] (client n k)))
 
     (invoke! [this test op]
       (timeout 5000 (assoc op :type :info, :error :timeout)
         (case (:f op)
           :read  (let[resp (-> (mon-get! node k))]
                    (assoc op :type :ok, :value resp))
-          :write ((mon-set! node k (:value op))
-                  (assoc op :type :ok)))))
+          ;:write ((mon-set! node k (:value op))
+	  :write (let[resp(-> (mon-set! node k (:value op)))]
+                  (assoc op :type :ok, :value resp))
+	)))
 
     (teardown! [_ test])))
 
@@ -49,7 +53,7 @@
   (assoc tests/noop-test
      :name "cephmon"
      :nemesis (nemesis/partition-random-halves)
-     :client (client nil nil)
+     :client (client nil nil) 
      :generator (->> (gen/mix [r w])
                      (gen/stagger 1)
                      (gen/nemesis
@@ -57,5 +61,7 @@
                                         {:type :info, :f :start}
                                         (gen/sleep 5)
                                         {:type :info, :f :stop}])))
-                     (gen/time-limit 60))
-     :model (model/set)))
+                     (gen/time-limit 45))
+     ;:model (model/set)
+     :checker checker/linearizable
+))
